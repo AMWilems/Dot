8e93cd335aa9e3e5c208f0357476b45381eec045
AMWilems <alexander.wilems@gmail.com>
2023-02-21 14:29:31 -0600

    no message

--- Console_Text.py
+++ Console_Text.py
@@ -4,5 +4,5 @@ import datetime
 #file for console calls when messaging user
 def GetTime():
     today = datetime.datetime.now()
-    dateTime = today.strftime("%m/%d/%Y, %H:%M:%S:")
+    dateTime = today.strftime("%m/%d/%Y %H:%M:%S:")
     return dateTime
