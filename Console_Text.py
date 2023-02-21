9f7a9b7344bd086cb0ca84d8c39defbd4eb03c4b
AMWilems <alexander.wilems@gmail.com>
2023-02-21 13:59:31 -0600

    added file for console text to help with terminal logs for debugging purposes

---
+++ Console_Text.py
@@ -0,0 +1,8 @@
+import os
+import datetime
+
+#file for console calls when messaging user
+def GetTime():
+    today = datetime.datetime.now()
+    dateTime = today.strftime("%m/%d/%Y, %H:%M:%S")
+    return dateTime
