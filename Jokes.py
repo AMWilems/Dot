97c3b60ae9b2b5dfe752ad2d07970c562a41905f
me <amw271@txstate.edu>
2023-04-13 15:17:52 -0500

    api change, other is paid

--- Jokes.py
+++ Jokes.py
@@ -1,11 +1,12 @@
 import requests
 
-url = "https://jokes.p.rapidapi.com/joke/random"
+url = "https://jokes-by-api-ninjas.p.rapidapi.com/v1/jokes"
 
 headers = {
 	"X-RapidAPI-Key": "3e8789e929msh7d6211db050d85cp146690jsneccd5efcbf28",
-	"X-RapidAPI-Host": "jokes.p.rapidapi.com"
+	"X-RapidAPI-Host": "jokes-by-api-ninjas.p.rapidapi.com"
 }
+
 def get_joke():
     response = requests.request("GET", url, headers=headers)
     return(response.text)
