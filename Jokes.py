7190ccfd9380a200099397b60b5292a3bb811b3d
me <amw271@txstate.edu>
2023-04-13 15:41:59 -0500

    different api test

--- Jokes.py
+++ Jokes.py
@@ -1,12 +1,13 @@
 import requests
+import json
 
-url = "https://jokes-by-api-ninjas.p.rapidapi.com/v1/jokes"
+url = "https://sus.p.rapidapi.com/api/"
 
 headers = {
 	"X-RapidAPI-Key": "3e8789e929msh7d6211db050d85cp146690jsneccd5efcbf28",
-	"X-RapidAPI-Host": "jokes-by-api-ninjas.p.rapidapi.com"
+	"X-RapidAPI-Host": "sus.p.rapidapi.com"
 }
 
 def get_joke():
     response = requests.request("GET", url, headers=headers)
-    return(response.text)
+    return(json.dumps(response.text))
